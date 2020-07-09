4428c85ac2d20b4d16c76a149b5b4371b1e2def8
JeffNaef-Trigermesh <jeff@triggermesh.com>
2020-07-09 18:15:27 -0400

    Removes const: 'ceID', 'rOK', and 'rAuthFailed' from zendesk.go PR comment-> https://github.com/triggermesh/knative-sources/pull/31#discussion_r452198297 , https://github.com/triggermesh/knative-sources/pull/31#discussion_r452317586 , and https://github.com/triggermesh/knative-sources/pull/31#discussion_r452343669 | Removes 'event.go' and moves back to zendesk.Ticket{} for the expected data.

--- zendesk/pkg/adapter/zendesk.go
+++ zendesk/pkg/adapter/zendesk.go
@@ -70,7 +70,7 @@ func NewZendeskAPIHandler(ceClient cloudevents.Client, port int, username, passw
 
 // Start the server for receiving Zendesk events. Will block until the stop channel closes.
 func (h *zendeskAPIHandler) Start(stopCh <-chan struct{}) error {
-	h.logger.Info("Starting Zendesk event handler..")
+	h.logger.Info("Starting Zendesk event handler...")
 
 	m := http.NewServeMux()
 	m.HandleFunc("/", h.handleAll)
